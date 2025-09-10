d3ec8346fb9ce87c587aed13d17aceeb822fcb3e
Vairam2007 <vairamuthu21072007@gmail.com>
2025-09-11 03:26:41 +0530

    one

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "gray-850": "#1f1f1f", // custom darker card color
-      },
-      backgroundImage: {
-        "dark-leaves": "url('https://www.transparenttextures.com/patterns/dark-leaves.png')",
-      },
-    },
-  },
-  plugins: [],
-};
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
